5cc65dd57da111231a68446b5f15c9642d1a16b0
George Entenman <zabouti@gmail.com>
2014-06-21 15:23:52 -0400

    added one of Iris' poems

---
+++ newpost.rb
@@ -0,0 +1,88 @@
+#!/usr/bin/env ruby
+
+# I think I'm trying to make a version of newpost that can be run from outside the jekyll directory.  The reason is that files in the jekyll directory are copied to the server.  The key to the new version is the variable SRCDIR
+
+DEBUG = false
+SRCDIR = %q{/Users/ge/Dropbox/ge/george.entenman.name.jekyll2/george.entenman.name}
+POSTSDIR = %Q{#{SRCDIR}/_posts}
+
+unless ARGV[1]
+  puts 'Usage: newpost "the post title" category {subcategory*}'
+  exit(-1)
+end
+ 
+# computed values
+	postdate = Time.now.strftime("%Y-%m-%d")
+	posttimestamp = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+
+	# posttitle = ARGV.join(" ")
+	posttitle = ARGV[0]
+	postname = ARGV[0].gsub(" ","-").gsub(",","").downcase
+  
+
+	#categories = %q{TBD}
+	categories = ARGV[1..-1]
+	main_category = ARGV[1] || %q{TBD}
+
+	postfilename = %Q{#{postdate}-#{postname}.md}
+
+	postbasesubpath = %Q{/#{main_category}/#{postdate}-#{postname}}
+	postsubpath = %Q{#{postbasesubpath}.md}
+	permalink = %Q{#{postbasesubpath}.html}
+
+	postrelpathname = %Q{_posts#{postsubpath}}
+	postfullpathname = %Q{#{SRCDIR}/#{postrelpathname}}
+
+
+initialfilecontents = <<-EOF
+---
+layout: post
+title: #{posttitle}
+date: #{posttimestamp}
+categories: #{categories}
+permalink: #{permalink}
+---
+
+<!-- EXCERPT HERE -->
+
+<!--more-->
+
+## Highest-Level Header in this file.
+
+EOF
+
+debug_message = <<-EOF
+	postdate: #{postdate}
+	posttimestamp: #{posttimestamp}
+
+	post title:  #{posttitle}
+
+	SRCDIR: #{SRCDIR}
+	POSTSDIR: #{POSTSDIR}
+
+	postfilename: #{postfilename}
+
+	postbasesubpath: #{postbasesubpath}
+	postsubpath: #{postsubpath}
+	permalink: #{permalink}
+
+	postrelpathname: #{postrelpathname}
+	postfullpathname: #{postfullpathname}
+
+
+Creating post file [#{postfullpathname}]
+Contents of the file:
+#{initialfilecontents}
+EOF
+
+if DEBUG
+	STDERR.puts debug_message
+else
+  File.open(postfullpathname,'w') do |f|
+  	f.puts %Q{#{initialfilecontents}}
+  end
+  STDERR.puts %Q{Have created #{postrelpathname}}
+end
+
+
+__END__
